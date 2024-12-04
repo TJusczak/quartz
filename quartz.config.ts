e6a9ddbf8d883b779e0c68b544a8ada568eefebc
Tyler Jusczak <jusczak@pm.me>
2024-12-04 14:34:00 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Second Brain - Quartz",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
