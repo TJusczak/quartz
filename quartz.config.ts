7979098244e2a15a497b06d813b107491ff00825
Tyler Jusczak <jusczak@pm.me>
2024-12-04 15:09:04 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: ":ledger: Second Brain",
+    pageTitle: "📒Second Brain",
     pageTitleSuffix: "",
     enableSPA: false,
     enablePopovers: true,
